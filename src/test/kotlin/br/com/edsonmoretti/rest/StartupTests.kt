ab60734e3b4cb06c9a2365135470daccef13f8aa
Edson Moretti do Nascimento <edsonmoretti@live.com>
2022-06-06 22:03:11 -0300

    start

---
+++ src/test/kotlin/br/com/edsonmoretti/rest/StartupTests.kt
@@ -0,0 +1,13 @@
+package br.com.edsonmoretti.rest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
